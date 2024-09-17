b6875bb6250f22de08ef57dea10762a4c16b09df
HamnaShafiq <hamnashafiq012@gmail.com>
2024-09-17 16:40:15 +0500

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -15,10 +15,10 @@ export default function Document() {
                     rel="stylesheet"
                 />
                 {/* Libraries Stylesheet */}
-                <link href="lib/animate/animate.min.css" rel="stylesheet" />
-                <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+                {/*<link href="lib/animate/animate.min.css" rel="stylesheet" />*/}
+                {/*<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />*/}
                 {/* Customized Bootstrap Stylesheet */}
-                <link href="css/style.css" rel="stylesheet" />
+                {/*<link href="css/style.css" rel="stylesheet" />*/}
             </Head>
             <body>
             <Main />
@@ -26,13 +26,13 @@ export default function Document() {
             {/* JavaScript Libraries */}
             <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-            <script src="lib/easing/easing.min.js"></script>
-            <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+            {/*<script src="lib/easing/easing.min.js"></script>*/}
+            {/*<script src="lib/owlcarousel/owl.carousel.min.js"></script>*/}
             {/* Contact JavaScript File */}
-            <script src="mail/jqBootstrapValidation.min.js"></script>
-            <script src="mail/contact.js"></script>
+            {/*<script src="mail/jqBootstrapValidation.min.js"></script>*/}
+            {/*<script src="mail/contact.js"></script>*/}
             {/* Template JavaScript */}
-            <script src="js/main.js"></script>
+            {/*<script src="js/main.js"></script>*/}
             </body>
         </Html>
     );
